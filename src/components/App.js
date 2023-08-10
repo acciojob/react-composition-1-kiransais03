5ddb2543d3ed1327c865faf2914298d886bf226f
kiransai <kiransais03@gmail.com>
2023-08-10 13:33:37 +0530

    submitting code-Thu Aug 10 2023 13:33:34 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import './../styles/App.css';
 import Tabs from "./Tabs"
 
 const App = () => {
-  let [tabs,setTabs] = useState([{title:"Tab 1",content:"Content for Tab 1"},{title:"Tab 2",content:"Content for Tab 2"},{title:"Tab 3",content:"Content for Tab 3"},{title:"Tab A",content:"Content for Tab A"},{title:"Tab B",content:"Content for Tab B"},{title:"Tab C",content:"Content for Tab C"}])
+  let [tabs,setTabs] = useState([{title:"Tab 1",content:"This is the Content for Tab 1"},{title:"Tab 2",content:"This is the Content for Tab 2"},{title:"Tab 3",content:"This is the Content for Tab 3"},{title:"Tab A",content:"This is the Content for Tab A"},{title:"Tab B",content:"This is the Content for Tab B"},{title:"Tab C",content:"This is the Content for Tab C"}])
   return (
     <div>
         {/* Do not remove the main div */}
