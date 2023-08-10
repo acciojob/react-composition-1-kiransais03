fbdd5643c06693d93e87752b20b5bb6e3f624c3f
kiransai <kiransais03@gmail.com>
2023-08-10 13:32:14 +0530

    submitting code-Thu Aug 10 2023 13:32:11 GMT+0530 (India Standard Time)

---
+++ src/components/Tabs.js
@@ -0,0 +1,17 @@
+import React,{useState} from "react";
+
+const Tabs = ({tabs,setTabs}) => {
+    let [content1,setContent1] = useState("Content for Tab 1");
+  return (
+    <div>
+        {/* Do not remove the main div */}
+        <ul>
+      {tabs.map((currelement,index,array) => {if(index>=3){return null} return (<li key={index} onClick={(event)=>{console.log(currelement.content); setContent1(currelement.content)}}>{currelement.title}</li>)})}
+      {content1 &&  <p>{content1}</p>}
+     
+      </ul>
+    </div>
+  )
+}
+
+export default Tabs
